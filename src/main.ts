176e4f791d3e3b13c85f28b66acb15e096a68d22
Clémence Meyer <clemence.meyer@gmail.com>
2025-03-04 16:55:04 +0100

    creation de la liste

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,84 @@
 import './style.css'
-import {io} from 'socket.io-client'
+import { io } from 'socket.io-client'
 class Home {
   pseudoValue: string
   buttonReady: HTMLElement | null
   inputPseudo: HTMLElement | null
   buttonJoin: HTMLElement | null
+  playerList: HTMLElement | null
   socket: any
 
-  constructor(){
+  constructor() {
+    
     this.socket = io("http://localhost:3000/")
-    this.pseudoValue=""
-    this.inputPseudo= document.getElementById("pseudoInput")
-
-    this.buttonJoin= document.getElementById("join")
-    this.buttonReady= document.getElementById("ready")
+    this.pseudoValue = ""
+    this.inputPseudo = document.getElementById("pseudoInput")
+    
+    this.playerList = document.getElementById("playerList")!
+    this.buttonJoin = document.getElementById("join")
+    this.buttonReady = document.getElementById("ready")
     this.initEvents()
     this.socket.on('joinGameStatus', this.displayJoin.bind(this))
+    this.socket.on("updatePlayers", this.handleUpdatePlayers.bind(this))
     
 
+
   }
-initEvents(){
-this.inputPseudo?.addEventListener("input",this.inputChange.bind(this))
-console.log(this.inputPseudo)
+  initEvents() {
+    this.inputPseudo?.addEventListener("input", this.inputChange.bind(this))
+    console.log(this.inputPseudo)
 
-this.buttonJoin?.addEventListener("click",this.clickJoin.bind(this))
+    this.buttonJoin?.addEventListener("click", this.clickJoin.bind(this))
+    this.buttonReady?.addEventListener("click", this.clickOnReady.bind(this))
 
-}
-inputChange(e){
-  this.pseudoValue=e.target.value
+  }
+  inputChange(e: any) {
+    this.pseudoValue = e.target.value
 
+  }
+
+  clickJoin() {
+    this.socket.emit('joinGame', this.pseudoValue)
+  }
+  displayJoin(message: any) {
+    console.log(message)
+  }
+
+
+  handleUpdatePlayers(players: any) {
+    this.playerList!.innerHTML=""
+    players.forEach((player: any) => {
+      console.log(player.name)
+      this.createPlayers(player)
+    }
+
+    )
+  }
+  createPlayers(player:any) {
+    const playerLi = document.createElement("li");
+    const nameP = document.createElement("p");
+    const readyP = document.createElement("p");
+    playerLi.appendChild(nameP);
+    playerLi.appendChild(readyP);
+    this.playerList?.appendChild(playerLi)
+    console.log(player)
+    nameP.innerText = player.name
+    if  (player.isReady){
+      readyP.innerText= "Prêt !"
+    }
+    else{
+      readyP.innerText= " pas Prêt !"
+    }
+
+
+  }
+
+  clickOnReady(){
+    this.socket.emit('playerReady')
+
+  }
 }
 
-clickJoin(){
-  this.socket.emit('joinGame', this.pseudoValue)
-}
-displayJoin(message: any){
-console.log(message)
-}
-}
 
 
 
